6e61f8eedb9204b70f9326996fa6185534fef109
cdustin <coledustin28@gmail.com>
2022-03-13 12:05:02 -0500

    2nd commit

--- ChessGame.py
+++ ChessGame.py
@@ -1,3 +1,14 @@
 import pygame
 
-#WIN = pygame.
+WIDTH = 800
+HEIGHT = 800
+
+WHITE =(255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN =(0,255,0)
+YELLOW = (0,0,255)
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
